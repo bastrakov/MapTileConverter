2da56c3aa256e5ceade787bf32e9fa6e293b45c1
Bastrakov Oleg <bastrakov@gmail.com>
2019-12-26 11:22:10 +0300

    console app

--- TileConverter/TileWorker/Program.cs
+++ TileConverter/TileWorker/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using System.Configuration;
 
 namespace TileWorker
 {
@@ -6,7 +8,29 @@ namespace TileWorker
 		{
 				static void Main(string[] args)
 				{
-						Console.WriteLine("Hello World!");
+
+						Console.WriteLine("InTilesDir: " + ConfigurationManager.AppSettings["InTilesDir"]);
+						var inTilesDir = ConfigurationManager.AppSettings["InTilesDir"].ToString();
+
+						Console.WriteLine("InXYZPathFormat: " + ConfigurationManager.AppSettings["InXYZPathFormat"]);
+						var inXYZPathFormat = ConfigurationManager.AppSettings["InXYZPathFormat"].ToString();
+
+						Console.WriteLine("OutTilesDir: " + ConfigurationManager.AppSettings["OutTilesDir"]);
+						var outTilesDir = ConfigurationManager.AppSettings["OutTilesDir"].ToString();
+
+						Console.WriteLine("OutXYZPathFormat: " + ConfigurationManager.AppSettings["OutXYZPathFormat"]);
+						var outXYZPathFormat = ConfigurationManager.AppSettings["OutXYZPathFormat"].ToString();
+
+
+						Console.WriteLine(ConfigurationManager.AppSettings["FromTo"]);
+						var fromTo = ConfigurationManager.AppSettings["FromTo"].ToString().ToLower();
+
+						if (fromTo == "spherical-wgs84")
+								new TileConverter().ConvertFromSphericalToWgs84(inTilesDir, inXYZPathFormat, outTilesDir, outXYZPathFormat);
+						else if (fromTo == "wgs84-spherical")
+								new TileConverter().ConvertFromWgs84ToSpherical(inTilesDir, inXYZPathFormat, outTilesDir, outXYZPathFormat);
+						
+						Console.WriteLine("TileConverter DONE");
 				}
 		}
 }
